2610f7bcbed145c5145991c668e1012058acd00c
cobear333 <haechan1223@gmail.com>
2023-05-03 16:14:46 +0900

    프로젝트 파일 생성

---
+++ JUKEA/src/main/java/com/example/jukea/JukeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.jukea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JukeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JukeaApplication.class, args);
+	}
+
+}
---
+++ JUKEA/src/test/java/com/example/jukea/JukeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jukea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JukeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
