192218e0a30653d5a4b4578e292e25814943fffa
cobear333 <haechan1223@gmail.com>
2023-06-07 13:51:51 +0900

    amount -> damount로 수정 원산지 추가.

--- JUKEA/src/main/java/com/example/jukea/dto/DrinkInfoDto.java
+++ JUKEA/src/main/java/com/example/jukea/dto/DrinkInfoDto.java
@@ -7,11 +7,12 @@ public class DrinkInfoDto {
 	private int did;			//고유 PK 자동증가	ex) 5
 	private String dname;	//술 이름			ex) 매화
 	private int dprice;		//술 가격			ex)	4500
-	private int amount;		//술 양			ex) 360
+	private int damount;		//술 양			ex) 360
 	private float dalchol;	//술 도수			ex) 7.5
 	private String dtype;	//술 종류			ex) 소주
 	private int dflavor; 	//쓴 정도 (0~9)	ex) 4
 	private String dsmell;	//술 향			ex) 매실향
 	private String dinfo;	//간단한 술 설명	ex) 매화수는 어쩌구 저쩌
 	private String dimage;	//술 이미지 링크	ex) maehwa.png
+	private String dorigin;
 }
