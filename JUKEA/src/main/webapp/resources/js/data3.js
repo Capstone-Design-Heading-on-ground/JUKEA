3f6c8d2e1decc0cd55c7185836cf67e9f571018c
cobear333 <haechan1223@gmail.com>
2023-06-09 01:21:35 +0900

    술비티아이 질문답변 수정

--- JUKEA/src/main/webapp/resources/js/data3.js
+++ JUKEA/src/main/webapp/resources/js/data3.js
@@ -10,6 +10,7 @@ const qnaList = [
         { answer: '오늘은 기분전환 브랜디', type: [0], value: 4},
         { answer: '맛있는 술 리큐르', type: [0], value: 5},
         { answer: '달리는날 위스키', type: [0], value: 6},
+        { answer: '상관없어', type: [0], value: 9},
       ]
     },
     {
@@ -17,16 +18,18 @@ const qnaList = [
       a: [
         { answer: '천천히 깊고 짙어지게 퍼지는 과일향 ', type: [1], value:0 },
         { answer: '은은하게 퍼지는 곡물 향', type: [1], value:1},
-        { answer: '강렬하게 톡 쏘는 향', type: [1], value:2 },
-        { answer: '술에 무슨 향이여, 그냥 알코올향', type: [1], value:3}
+        { answer: '개성있는 특유의 향', type: [1], value:2 },
+        { answer: '술에 무슨 향이여, 그냥 알코올향', type: [1], value:3},
+        { answer: '상관없어', type: [1], value: 9},
       ]
     },
     {
       q: '3. 술맛이 어느정도 나면 적당해?',
       a: [
-        { answer: '진하고 깊은 맛', type: [2], value:0},
-        { answer: '깔끔하고 청량한 맛', type: [2], value:1},
-        { answer: '톡 쏘며 신맛', type: [2], value:2}
+        { answer: '맛있는 술이였으면 좋겠어', type: [2], value:0},
+        { answer: '적당히 술맛도나고 달달하기도 하고', type: [2], value:1},
+        { answer: '그냥 술맛이 좋아', type: [2], value:2},
+        { answer: '상관없어', type: [2], value: 9},
       ]
     },
     {
@@ -36,6 +39,7 @@ const qnaList = [
         { answer: '5000원에서 10000원정도!',type: [3], value:1},
         { answer: '10000원에서 50000원 사이', type: [3], value:2},
         { answer: '50000원이상!!!', type: [3], value:3},
+        { answer: '상관없어', type: [3], value: 9},
       ]
     },
     {
@@ -44,6 +48,7 @@ const qnaList = [
         { answer: '나는 술찌 (5도 이하)', type: [4], value:0},
         { answer: '나는 소주정도는 마시지 (5~22도)', type: [4], value:1},
         { answer: '독한술도 잘마셔 (22~50도)', type: [4], value:2},
+        { answer: '상관없어', type: [4], value: 9},
       ]
     }
   ]
